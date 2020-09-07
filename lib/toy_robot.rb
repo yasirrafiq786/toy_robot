d3272fd40a579eb7bb96bfc7135289c876832f03
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-08 00:02:28 +1000

    made the application

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -3,10 +3,9 @@ class ToyRobot
 
   X = (0..4).to_a
   Y = (0..4).to_a
-  DIRECTIONS = %w(north east south west)
+  DIRECTIONS = %w[north east south west].freeze
 
-  def initialize
-  end
+  def initialize; end
 
   def place(x, y, f)
     if inside_table?(x, y)
@@ -24,15 +23,57 @@ class ToyRobot
     when "east"
       @position[0] == 4 ? nil : @position[0] += 1
     when "south"
-      @position[1] == 0 ? nil : @position[1] -= 1
+      (@position[1]).zero? ? nil : @position[1] -= 1
     when "west"
-      @position[0] == 0 ? nil : @position[0] -= 1
+      (@position[0]).zero? ? nil : @position[0] -= 1
     else
-      puts " Not a valid input"
+      puts "Robot has not been placed on the table or you have entered an invalid input"
+    end
+  end
+
+  def left
+    case @facing
+    when "north"
+      self.facing = "west"
+    when "west"
+      self.facing = "south"
+    when "south"
+      self.facing = "east"
+    when "east"
+      self.facing = "north"
+    else
+      puts "Robot has not been placed on the table or you have entered an invalid input"
+    end
+  end
+
+  def right
+    case @facing
+    when "north"
+      self.facing = "east"
+    when "east"
+      self.facing = "south"
+    when "south"
+      self.facing = "west"
+    when "west"
+      self.facing = "north"
+    else
+      puts "Robot has not been placed on the table or you have entered an invalid input"
+    end
+  end
+
+  def report
+    if on_the_table?
+      [@position[0], @position[1], @facing]
+    else
+      puts "Robot has not been placed on the table"
     end
   end
 
   def inside_table?(x, y)
     X.include?(x) && Y.include?(y)
+  end
+
+  def on_the_table?
+    @position.nil? || @facing.nil? ? false : true
   end
 end
--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -3,6 +3,12 @@ require "toy_robot"
 describe ToyRobot do
   let(:toy_robot) { ToyRobot.new }
 
+  describe "Has Robot been placed on the table" do
+    it "Should return false if robot has not been assigned a position or direction" do
+      expect(toy_robot.on_the_table?).to eq false
+    end
+  end
+
   describe "Is the toy robot inside the table" do
     it "Should return true if robot is inside the table" do
       expect(toy_robot.inside_table?(0, 0)).to eq true
@@ -24,6 +30,31 @@ describe ToyRobot do
       toy_robot.place(0, 0, "north")
       toy_robot.move
       expect(toy_robot.position).to eq [0, 1]
+    end
+  end
+
+  describe "Rotating the robot" do
+    it "LEFT command should rotate robot at its place 90 degree counter clockwise" do
+      toy_robot.place(1, 1, "north")
+      toy_robot.left
+      expect(toy_robot.facing).to eq "west"
+    end
+
+    it "RIGHT command should rotate robot at its place 90 degree clockwise" do
+      toy_robot.place(2, 1, "south")
+      toy_robot.right
+      expect(toy_robot.facing).to eq "west"
+    end
+  end
+
+  describe "Reporting the position" do
+    it "Should report its current position" do
+      toy_robot.place(1, 2, "east")
+      toy_robot.move
+      toy_robot.move
+      toy_robot.left
+      toy_robot.move
+      expect(toy_robot.report).to eq [3, 3, "north"]
     end
   end
 end
